ba00922f5b94e30b6c6dfc69d712d16c23ff70c0
prashantkc09 <114799837+prashantkc09@users.noreply.github.com>
2022-10-16 11:33:20 +0530

    completed a program

---
+++ greatestnumber.py
@@ -0,0 +1,11 @@
+#take two inputs from users
+number1=int(input("enter the number1-"))
+number2=int(input("enter the number2-"))
+
+if number1>number2:
+    print("number 1 is greatest")
+elif number2>number1:
+    print("number 2 is greatest")
+
+else:
+    print("both number are same")
