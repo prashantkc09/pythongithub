bfce350ca6095cbc3f5653e6f836ea272a3989ec
prashantkc09 <114799837+prashantkc09@users.noreply.github.com>
2022-10-26 23:24:23 +0530

    program practice-4

---
+++ practice-4.py
@@ -0,0 +1,6 @@
+Attentedclass=float(input("how many class u have attented"))
+totalclass=float(input("total lec in your course"))
+
+currentattendence=Attentedclass/totalclass*100
+
+print(currentattendence)
