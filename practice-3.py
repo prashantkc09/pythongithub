dd34047324d0750a6f67708b461d972c848bbca2
prashantkc09 <114799837+prashantkc09@users.noreply.github.com>
2022-10-26 23:23:31 +0530

    program practice-3

---
+++ practice-3.py
@@ -0,0 +1,7 @@
+currentsalary=float(input("what is the current salary"))
+yearofservice=float(input("how many years you have worked"))
+
+if yearofservice>5:
+    print(currentsalary + 1000)
+else:
+    print(currentsalary)
