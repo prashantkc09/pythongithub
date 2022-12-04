55582552d2ed1b0c5567d1cb4c2c5cfd541863ca
prashantkc09 <114799837+prashantkc09@users.noreply.github.com>
2022-12-04 23:58:41 +0530

    Add files via upload

---
+++ topic-3.py
@@ -0,0 +1,9 @@
+#list1=[10,20,30,40,50]
+#triple all the elements in the list"
+# list1 = [10, 20, 30, 40, 50]
+# triple all the elements in the list
+
+list1 = [10, 20, 30, 40, 50]
+for i in range(len(list1)):
+    list1[i] = list1[i] * 3
+print(list1)
